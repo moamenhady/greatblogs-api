24164c9b462b380eee21ccf394d0115637bdfcd0
Moamen Hady <moamen.hady@outlook.com>
2023-12-25 01:37:27 +0200

    Initial commit

---
+++ src/main/java/dev/moamenhady/greatblogsapi/GreatblogsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.moamenhady.greatblogsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatblogsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreatblogsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/moamenhady/greatblogsapi/GreatblogsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.moamenhady.greatblogsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatblogsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
