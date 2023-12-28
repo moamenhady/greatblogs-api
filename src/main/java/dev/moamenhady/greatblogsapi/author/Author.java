df3e274ae95f4fda835dcf31833ad6d0bd73ad3a
Moamen Hady <moamen.hady@outlook.com>
2023-12-28 04:55:44 +0200

    Update Author class

--- src/main/java/dev/moamenhady/greatblogsapi/author/Author.java
+++ src/main/java/dev/moamenhady/greatblogsapi/author/Author.java
@@ -4,6 +4,7 @@ import dev.moamenhady.greatblogsapi.post.Post;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 
@@ -105,5 +106,46 @@ public class Author {
 
     public void setAbout(String about) {
         this.about = about;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (!Objects.equals(id, author.id)) return false;
+        if (!Objects.equals(username, author.username)) return false;
+        if (!Objects.equals(password, author.password)) return false;
+        if (!Objects.equals(email, author.email)) return false;
+        if (!Objects.equals(fullName, author.fullName)) return false;
+        if (!Objects.equals(about, author.about)) return false;
+        return Objects.equals(posts, author.posts);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (fullName != null ? fullName.hashCode() : 0);
+        result = 31 * result + (about != null ? about.hashCode() : 0);
+        result = 31 * result + (posts != null ? posts.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", about='" + about + '\'' +
+                ", posts=" + posts +
+                '}';
     }
 }
