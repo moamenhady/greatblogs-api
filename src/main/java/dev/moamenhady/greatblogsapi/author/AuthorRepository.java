f9f21592420701121fe013edf76914d3946b9f7d
Moamen Hady <moamen.hady@outlook.com>
2024-01-04 10:10:19 +0200

    Update AuthorRepository

--- src/main/java/dev/moamenhady/greatblogsapi/author/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.moamenhady.greatblogsapi.author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
